3dd99ecaee6f0efadc24cc9b443651b756d5fb30
Anton <aner-anton@yandex.ru>
2023-09-02 11:50:28 +0300

    submenus

--- resources/views/components/admin/menu/index.blade.php
+++ resources/views/components/admin/menu/index.blade.php
@@ -20,24 +20,26 @@
 					Продажи видео
 				</div>
 			</x-admin.menu.subitem>
-			<x-admin.menu.subitem stats="metrics-sails" page="sum" title="Суммарный график продаж">
-				<img width="25px" src="{{ Vite::asset('resources/images/icon5.png') }}" alt="">
-				<div>
-					Суммарный график продаж
-				</div>
-			</x-admin.menu.subitem>
-			<x-admin.menu.subitem stats="metrics-sails" page="audio" title="График продаж аудио">
-				<img width="25px" src="{{ Vite::asset('resources/images/icon5.png') }}" alt="">
-				<div>
-					График продаж аудио
-				</div>
-			</x-admin.menu.subitem>
-			<x-admin.menu.subitem stats="metrics-sails" page="video" title="График продаж видео">
-				<img width="25px" src="{{ Vite::asset('resources/images/icon5.png') }}" alt="">
-				<div>
-					График продаж видео
-				</div>
-			</x-admin.menu.subitem>
+			<x-admin.menu.submenu title="Графики продаж" img="{{ Vite::asset('resources/images/icon5.png') }}">
+				<x-admin.menu.subitem stats="metrics-sails" page="sum" title="Суммарный график продаж">
+					<img width="25px" src="{{ Vite::asset('resources/images/icon1.png') }}" alt="">
+					<div>
+						Суммарный график продаж
+					</div>
+				</x-admin.menu.subitem>
+				<x-admin.menu.subitem stats="metrics-sails" page="audio" title="График продаж аудио">
+					<img width="25px" src="{{ Vite::asset('resources/images/icon2.png') }}" alt="">
+					<div>
+						График продаж аудио
+					</div>
+				</x-admin.menu.subitem>
+				<x-admin.menu.subitem stats="metrics-sails" page="video" title="График продаж видео">
+					<img width="25px" src="{{ Vite::asset('resources/images/icon3.png') }}" alt="">
+					<div>
+						График продаж видео
+					</div>
+				</x-admin.menu.subitem>
+			</x-admin.menu.submenu>
 			<x-admin.menu.subitem stats="geo" page="sum" title="Геолокация продаж">
 				<img width="25px" src="{{ Vite::asset('resources/images/icon6.png') }}" alt="">
 				<div>
@@ -53,36 +55,43 @@
 					Суммарные просмотры и прослушивания
 				</div>
 			</x-admin.menu.subitem>
+
 			<x-admin.menu.subitem stats="views" page="audio" title="Прослушивания аудио">
 				<img width="25px" src="{{ Vite::asset('resources/images/icon2.png') }}" alt="">
 				<div>
 					Прослушивания аудио
 				</div>
 			</x-admin.menu.subitem>
+
 			<x-admin.menu.subitem stats="views" page="video" title="Просмотры видео">
 				<img width="25px" src="{{ Vite::asset('resources/images/icon3.png') }}" alt="">
 				<div>
 					Просмотры видео
 				</div>
 			</x-admin.menu.subitem>
-			<x-admin.menu.subitem stats="metrics-views" page="sum" title="Суммарный график просмотров и прослушиваний">
-				<img width="25px" src="{{ Vite::asset('resources/images/icon5.png') }}" alt="">
-				<div>
-					Суммарный график просмотров и прослушиваний
-				</div>
-			</x-admin.menu.subitem>
-			<x-admin.menu.subitem stats="metrics-views" page="audio" title="График прослушивания аудио">
-				<img width="25px" src="{{ Vite::asset('resources/images/icon5.png') }}" alt="">
-				<div>
-					График прослушивания аудио
-				</div>
-			</x-admin.menu.subitem>
-			<x-admin.menu.subitem stats="metrics-views" page="video" title="График просмотров видео">
-				<img width="25px" src="{{ Vite::asset('resources/images/icon5.png') }}" alt="">
-				<div>
-					График просмотров видео
-				</div>
-			</x-admin.menu.subitem>
+
+			<x-admin.menu.submenu title="Графики просмотров и прослушиваний" img="{{ Vite::asset('resources/images/icon5.png') }}">
+				<x-admin.menu.subitem stats="metrics-views" page="sum" title="Суммарный график просмотров и прослушиваний">
+					<img width="25px" src="{{ Vite::asset('resources/images/icon1.png') }}" alt="">
+					<div>
+						Суммарный график просмотров и прослушиваний
+					</div>
+				</x-admin.menu.subitem>
+
+				<x-admin.menu.subitem stats="metrics-views" page="audio" title="График прослушивания аудио">
+					<img width="25px" src="{{ Vite::asset('resources/images/icon2.png') }}" alt="">
+					<div>
+						График прослушивания аудио
+					</div>
+				</x-admin.menu.subitem>
+				
+				<x-admin.menu.subitem stats="metrics-views" page="video" title="График просмотров видео">
+					<img width="25px" src="{{ Vite::asset('resources/images/icon3.png') }}" alt="">
+					<div>
+						График просмотров видео
+					</div>
+				</x-admin.menu.subitem>
+			</x-admin.menu.submenu>
 		</x-admin.menu.item>
 
         <li class="flex items-center header__nav__list-item gap-x-1" @click="$dispatch('all-stats', 'all-audio')">
---
+++ resources/views/components/admin/menu/submenu.blade.php
@@ -0,0 +1,27 @@
+@props([
+	'title',
+	'img' => '',
+])
+
+<li 
+	class="relative flex items-center justify-between w-full px-3 py-2 cursor-pointer gap-x-1 hover:bg-primary"
+	x-data="{menu: false}"
+	@mouseover="menu = true"
+	@mouseout="menu = false"
+>
+	<div class="flex items-center gap-x-1">
+		<img width="25px" src="{{ $img }}" alt="">
+		<div>
+			{{ $title }}
+		</div>
+	</div>
+
+	<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
+		<path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+	</svg>
+
+
+	<ul class="absolute top-0 left-full bg-[#344760] rounded [&>*:nth-child(n)]:!ml-0 flex flex-col gap-y-3 whitespace-nowrap transition text-[1.2vw] shadow-md shadow-gray-800 min-w-[450px]" x-show="menu">
+		{{ $slot }}
+	</ul>
+</li>
