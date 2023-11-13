763e12167260bb87df1633748a5b56e284b14a19
Anton <aner-anton@yandex.ru>
2023-11-13 16:54:05 +0300

    Design page prices

--- resources/views/admin/prices.blade.php
+++ resources/views/admin/prices.blade.php
@@ -9,12 +9,12 @@
 				Цены
 			</span>
 			<div class="flex gap-8">
-				<div>Курс валюты:</div>
+				<div class="font-bold">Курс валюты:</div>
 				<div class="flex gap-1" title="Данные за {{ $usd->date->format('d.m.Y') }}. Обновлено {{ $usd->updated_at->diffForHumans() }}">
-					EUR 1 = USD {{ $usd->rate }}
+					<span class="text-secondary">EUR 1 = </span>USD {{ $usd->rate }}
 				</div>
 				<div class="flex gap-1" title="Данные за {{ $rub->date->format('d.m.Y') }}. Обновлено {{ $rub->updated_at->diffForHumans() }}">
-					EUR 1 = RUB {{ $rub->rate }}
+					<span class="text-secondary">EUR 1 = </span>RUB {{ $rub->rate }}
 				</div>
 			</div>
 			<div class="ml-auto burger">
@@ -29,7 +29,7 @@
 			<div class="text-lg text-black max-h-[calc(100vh-82px)] overflow-y-auto pb-2 flex flex-col gap-1">
 				<template x-for="fragment in fragments">
 					<div class="flex items-center justify-between px-2 py-3 transition gap-x-3 rounded-xl bg-white/20">
-						<div class="w-28" x-text="'Фрагмент ' + fragment.id"></div>
+						<div class="w-28" :class="fragment.id < 7 ? 'text-[#8f6143]' : 'text-secondary'" x-text="'Фрагмент ' + fragment.id"></div>
 						<div class="flex items-center gap-2 min-w-[450px]" x-data="{
 							euro: fragment.audio.price,
 							savedShow: false,
@@ -49,24 +49,28 @@
 									})
 							},
 						}">
-							<img class="w-[35px] h-[35px]" src="{{ Vite::asset('resources/images/icon2.png') }}" alt="Аудио">
-							<span>€</span>
+							<img class="w-auto h-[35px] opacity-40" src="{{ Vite::asset('resources/images/icon2.png') }}" alt="Аудио">
+							<span class="text-secondary">€</span>
 							<form>
 								<input 
-									class="w-16 p-1 text-center bg-transparent border border-t-0 border-black border-x-0 focus:outline-none focus-visible:outline-none" 
+									class="w-16 p-1 text-center bg-transparent border border-t-0 text-secondary border-secondary border-x-0 focus:outline-none focus-visible:outline-none" 
 									name="price" x-model="euro" 
 									@input.debounce.750ms="saveAudio(fragment.id)"
 								>
-								<input type="hidden" name="usd" :value="roundNumber(euro * usd.rate)">
-								<input type="hidden" name="rub" :value="roundNumber(euro * rub.rate)">
 							</form>
 							<div class="w-[32px] h-[32px]">
 								<div class="p-1 transition bg-green-500 rounded-full" x-show="savedShow" x-transition.opacity.duration.600ms>
 									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>
 								</div>
 							</div>
-							<span class="w-32" x-text="'$ ' + format(euro * usd.rate)"></span>
-							<span class="w-32" x-text="'₽ ' + format(euro * rub.rate)"></span>
+							<div class="flex w-[250px] overflow-hidden bg-white text-primary rounded-xl">
+								<span class="w-[44%] py-1 pl-5" x-text="'$ ' + format(euro * usd.rate)"></span>
+								<span 
+									class="w-[56%] py-1 pr-5 text-right text-white bg-primary" 
+									style="clip-path: polygon(12% 0, 100% 0, 100% 100%, 0% 100%);"
+									x-text="'₽ ' + format(euro * rub.rate)"
+								></span>
+							</div>
 						</div>
 
 						<div class="flex items-center gap-2 min-w-[450px]" x-data="{
@@ -88,25 +92,29 @@
 									})
 							},
 						}">
-							<img class="w-[35px] h-[35px]" src="{{ Vite::asset('resources/images/icon3.png') }}" alt="Видео">
-							<span>€</span>
+							<img class="w-auto h-[35px] opacity-40" src="{{ Vite::asset('resources/images/icon3.png') }}" alt="Видео">
+							<span class="text-secondary">€</span>
 							<form>
 								<input 
-									class="w-16 p-1 text-center bg-transparent border border-t-0 border-black border-x-0 focus:outline-none focus-visible:outline-none" 
+									class="w-16 p-1 text-center bg-transparent border border-t-0 text-secondary border-secondary border-x-0 focus:outline-none focus-visible:outline-none" 
 									name="price" 
 									x-model="euro" 
 									@input.debounce.750ms="saveVideo(fragment.id)"
 								>
-								<input type="hidden" name="usd" :value="roundNumber(euro * usd.rate)">
-								<input type="hidden" name="rub" :value="roundNumber(euro * rub.rate)">
 							</form>
 							<div class="w-[32px] h-[32px]">
 								<div class="p-1 transition bg-green-500 rounded-full" x-show="savedShow" x-transition.opacity.duration.600ms>
 									<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6"><path stroke-linecap="round" stroke-linejoin="round" d="M4.5 12.75l6 6 9-13.5" /></svg>
 								</div>
 							</div>
-							<span class="w-32" x-text="'$ ' + format(euro * usd.rate)"></span>
-							<span class="w-32" x-text="'₽ ' + format(euro * rub.rate)"></span>
+							<div class="flex w-[250px] overflow-hidden bg-white text-primary rounded-xl">
+								<span class="w-[44%] py-1 pl-5" x-text="'$ ' + format(euro * usd.rate)"></span>
+								<span 
+									class="w-[56%] py-1 pr-5 text-right text-white bg-primary" 
+									style="clip-path: polygon(12% 0, 100% 0, 100% 100%, 0% 100%);"
+									x-text="'₽ ' + format(euro * rub.rate)"
+								></span>
+							</div>
 						</div>
 
 					</div>
