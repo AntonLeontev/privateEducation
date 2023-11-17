cb892da434e4c78ef212146bb410a8d26ac5d742
Anton <aner-anton@yandex.ru>
2023-11-17 23:28:53 +0300

    colors in user history

--- resources/views/components/user.blade.php
+++ resources/views/components/user.blade.php
@@ -181,14 +181,14 @@
 											<div class="flex items-center min-w-[150px] pl-2" x-text="'Фрагмент №' + fragment.id"></div>
 											<div class="flex items-center gap-3 min-w-[180px]">
 												<img class="w-[45px] h-[35px]" src="{{ Vite::asset('resources/images/icon4.png') }}" alt="Презентация">
-												<div class="flex items-center gap-1 min-w-[50px]" :title="'Количество просмотров видео презентации фрагмента №' + fragment.id">
+												<div class="flex items-center gap-1 min-w-[50px] text-[#f3fa5e]" :title="'Количество просмотров видео презентации фрагмента №' + fragment.id">
 													<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 														<path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
 														<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 													</svg>
 													<span x-text="fragment.presentation.views"></span>
 												</div>
-												<div class="flex items-center gap-1" :title="'Количество прочтений текста презентации фрагмента  №' + fragment.id">
+												<div class="flex items-center gap-1 text-[#1caab7]" :title="'Количество прочтений текста презентации фрагмента  №' + fragment.id">
 													<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 														<path stroke-linecap="round" stroke-linejoin="round" d="M12 6.042A8.967 8.967 0 006 3.75c-1.052 0-2.062.18-3 .512v14.25A8.987 8.987 0 016 18c2.305 0 4.408.867 6 2.292m0-14.25a8.966 8.966 0 016-2.292c1.052 0 2.062.18 3 .512v14.25A8.987 8.987 0 0018 18a8.967 8.967 0 00-6 2.292m0-14.25v14.25" />
 													</svg>
@@ -199,7 +199,7 @@
 												<template x-if="fragment.audio">
 													<div class="grid items-center grid-flow-col gap-1 auto-cols-max">
 														<img class="w-[35px] h-[35px]" src="{{ Vite::asset('resources/images/icon2.png') }}" alt="Аудио">
-														<div class="flex items-center gap-1 min-w-[70px]" :title="'Количество прослушиваний аудио фрагмента  №' + fragment.id">
+														<div class="flex items-center gap-1 min-w-[70px] text-white" :title="'Количество прослушиваний аудио фрагмента  №' + fragment.id">
 															<svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-volume-up-fill" viewBox="0 0 16 16">
 																<path d="M11.536 14.01A8.473 8.473 0 0 0 14.026 8a8.473 8.473 0 0 0-2.49-6.01l-.708.707A7.476 7.476 0 0 1 13.025 8c0 2.071-.84 3.946-2.197 5.303l.708.707z"/>
 																<path d="M10.121 12.596A6.48 6.48 0 0 0 12.025 8a6.48 6.48 0 0 0-1.904-4.596l-.707.707A5.483 5.483 0 0 1 11.025 8a5.483 5.483 0 0 1-1.61 3.89l.706.706z"/>
@@ -207,7 +207,7 @@
 															</svg>
 															<span x-text="fragment.audio?.views"></span>
 														</div>
-														<div class="flex items-center gap-1 min-w-[80px]" :title="'Стоимость подписки аудио фрагмента №' + fragment.id">
+														<div class="flex items-center gap-1 min-w-[80px] text-secondary" :title="'Стоимость подписки аудио фрагмента №' + fragment.id">
 															<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 																<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 															</svg>
@@ -221,7 +221,7 @@
 																<path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>
 																<circle cx="11" cy="8" r="4" fill="#7e6a96"/>
 															</svg>
-															<span x-text="fragment.audio?.ago"></span>
+															<span class="text-secondary" x-text="fragment.audio?.ago"></span>
 														</div>
 													</div>
 												</template>
@@ -230,14 +230,14 @@
 												<template x-if="fragment?.video">
 													<div class="flex items-center gap-1">
 														<img class="w-[35px] h-[35px]" src="{{ Vite::asset('resources/images/icon3.png') }}" alt="Видео">
-														<div class="flex items-center gap-1 min-w-[70px]" :title="'Количество просмотров видео фрагмента  №' + fragment.id">
+														<div class="flex items-center gap-1 min-w-[70px] text-[#f3fa5e]" :title="'Количество просмотров видео фрагмента  №' + fragment.id">
 															<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 																<path stroke-linecap="round" stroke-linejoin="round" d="M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z" />
 																<path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
 															</svg>
 															<span x-text="fragment.video?.views"></span>
 														</div>
-														<div class="flex items-center gap-1 min-w-[80px]" :title="'Стоимость подписки видео фрагмента №' + fragment.id">
+														<div class="flex items-center gap-1 min-w-[80px] text-secondary" :title="'Стоимость подписки видео фрагмента №' + fragment.id">
 															<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 																<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 															</svg>
@@ -251,7 +251,7 @@
 																<path d="M5 3a5 5 0 0 0 0 10h6a5 5 0 0 0 0-10H5zm6 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z"/>
 																<circle cx="11" cy="8" r="4" fill="#7e6a96"/>
 															</svg>
-															<span x-text="fragment.video?.ago"></span>
+															<span class="text-secondary" x-text="fragment.video?.ago"></span>
 														</div>
 													</div>
 												</template>
