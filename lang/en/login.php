d8991c752edd39fe7c37f2011d02804884a11649
Anton <aner-anton@yandex.ru>
2024-07-15 18:01:18 +0300

    add: login translate

---
+++ lang/en/login.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'title' => 'Personal Account',
+    'auth' => 'Authorization',
+    'user' => 'I am a user',
+    'register' => 'Registration',
+    'email' => 'Email',
+    'password' => 'Password',
+    'forget' => 'Forgot your password?',
+    'login_btn' => 'ENTER',
+    'error_message' => 'Unable to log in. The email or password you entered is incorrect',
+    'restore' => 'Restore password',
+    'restore_btn' => 'RESTORE',
+    'new_password_message' => 'A new password has been sent to your email',
+    'new_password_btn' => 'AUTHORIZATION',
+    'register_btn' => 'CONTINUE',
+    'register_message' => 'To continue registration, follow the link in the email to confirm your email ',
+];
--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -16,7 +16,7 @@
 
                 <div id="dialog1" class="" style="visibility: visible">
                     <div class="dialog__top">
-                        <h4>Личный кабинет</h4>
+                        <h4>{{ __('login.title') }}</h4>
                         <a class="dialog__close" href="{{ route('home') }}">
 
                         </a>
@@ -25,85 +25,85 @@
                         <!-- для авторизации -->
                         <div id="account-autorization" class="dialog__autorization autorization">
                             <h3 class="autorization__title">
-                                Авторизация
+                                {{ __('login.auth') }}
                             </h3>
                             <div class="autorization__button-box">
                                 <button id="autorization-mode-btn-autorization"
                                     class="autorization__btn autorization__btn--autorization">
                                     <img src="/img/user.png" alt="user" class="autorization__img" />
-                                    <span class="autorization__btn-text">Я&nbsp;пользователь</span></button>
+                                    <span class="autorization__btn-text">{!! __('login.user') !!}</span></button>
                                 <button id="autorization-mode-btn-registration"
                                     class="autorization__btn autorization__btn--registration">
                                     <img src="/img/user.png" alt="user"
                                         class="autorization__img autorization__img--grey" />
-                                    <span class="autorization__btn-text">Регистрация</span></button>
+                                    <span class="autorization__btn-text">{{ __('login.register') }}</span></button>
                             </div>
                             <form id="autorization-form" class="autorization__form">
                                 <span class="autorization__label">
-                                    Адрес электронной почты:
+                                    {{ __('login.email') }}:
                                 </span>
                                 <input id="autorization-email-input" type="email" class="autorization__input"
                                     placeholder="* * * * * * * * * * * * * * * * *">
                                 <div class="autorization__wrapper">
                                     <span class="autorization__label">
-                                        Пароль:
+                                        {{ __('login.password') }}:
                                     </span>
                                     <button type="button" id="autorization-password-recall-btn" class="autorization__link">
-                                        Забыли пароль?
+                                        {{ __('login.forget') }}
                                     </button>
 
                                 </div>
                                 <input id="autorization-password-input" type="password" class="autorization__input"
                                     placeholder="* * * * * * * * * * * * * * * * *">
-                                <span id="autorization-error-msg" class="autorization__error-msg">Не удалось войти в
-                                    аккаунт, введенные
-                                    e-mail или пароль неверны</span>
+                                <span id="autorization-error-msg" class="autorization__error-msg">
+									{{ __('login.error_message') }}
+								</span>
                                 <button type="submit" id="autorization-submit-btn" class="autorization__submit-btn">
-                                    ВХОД
+                                    {{ __('login.login_btn') }}
                                 </button>
                             </form>
                         </div>
                         <!-- для регистрации -->
                         <div id="account-registration" class="dialog__registration registration">
                             <h3 class="registration__title">
-                                Регистрация
+                                {{ __('login.register') }}
                             </h3>
                             <div class="registration__button-box">
                                 <button id="registration-mode-btn-aurorization"
                                     class="registration__btn registration__btn--autorization">
                                     <img src="/img/user.png" alt="user"
                                         class="registration__img registration__img--grey" />
-                                    <span class="registration__btn-text">Авторизация</span></button>
+                                    <span class="registration__btn-text">{{ __('login.auth') }}</span></button>
                                 <button id="registration-mode-btn-registration"
                                     class="registration__btn registration__btn--registration">
                                     <img src="/img/user.png" alt="user" class="registration__img" />
-                                    <span class="registration__btn-text">Регистрация</span></button>
+                                    <span class="registration__btn-text">{{ __('login.register') }}</span></button>
                             </div>
                             <form id="registration-form" class="registration__form">
                                 <span class="registration__label">
-                                    Адрес электронной почты:
+                                    {{ __('login.email') }}:
                                 </span>
                                 <input id="registration-email-input" type="email" class="registration__input"
                                     placeholder="* * * * * * * * * * * * * * * * *">
                                 <button id="registration-submit-btn" class="registration__submit-btn">
-                                    ПРОДОЛЖИТЬ
+                                    {{ __('login.register_btn') }}
                                 </button>
                             </form>
                         </div>
                         <!-- напомнить пароль -->
                         <div id="account-password-recall" class="dialog__password-recall password-reacll">
                             <h3 class="password-reacll__title">
-                                Восстановить пароль
+                                {{ __('login.restore') }}
                             </h3>
 
                             <form id="password-reacll-form" class="password-reacll__form">
                                 <span class="password-reacll__label">
-                                    Адрес электронной почты:
+                                    {{ __('login.email') }}:
                                 </span>
                                 <input id="password-reacall-input" type="email" class="password-reacll__input"
                                     placeholder="* * * * * * * * * * * * * * * * *">
                                 <button id="password-reacll-submit-btn" class="password-reacll__submit-btn">
-                                    ВОССТАНОВИТЬ
+                                    {{ __('login.restore_btn') }}
                                 </button>
                             </form>
 
@@ -112,8 +112,7 @@
                         <div id="account-password-info-msg" class="dialog__password-info-msg password-info-msg">
                             <div class="password-info-msg__wrapper">
                                 <p class="password-info-msg__text">
-                                    Для продолжения регистрации перейдите
-                                    по&nbsp;ссылке в&nbsp;письме и&nbsp;подтвердите e-mail
+                                    {{ __('login.register_message') }}
                                 </p>
                             </div>
                         </div>
@@ -122,12 +121,11 @@
                             class="dialog__password-new-info-msg password-new-info-msg">
                             <div class="password-new-info-msg__wrapper">
                                 <p class="password-new-info-msg__text">
-                                    На&nbsp;вашу почту отправлен новый
-                                    пароль для входа на&nbsp;сервис
+                                    {{ __('login.new_password_message') }}
                                 </p>
                             </div>
                             <div id="password-new-info-msg-btn" class="password-new-info-msg__btn">
-                                АВТОРИЗАЦИЯ
+                                {{ __('login.new_password_btn') }}
                             </div>
                         </div>
                     </div>
