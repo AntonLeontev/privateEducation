7cf6bdb673af2c0cc80b68207238991c05c1c386
Anton <aner-anton@yandex.ru>
2024-07-21 00:46:38 +0300

    add: contacts translate

---
+++ lang/en/contacts.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+    'h1' => 'Contacts',
+    '1' => 'Контактная информация',
+    '2' => 'E-mail',
+    '3' => 'Telephone in Latvia',
+    '4' => 'Telephone in Germany',
+    '5' => 'Development Office',
+    '6' => 'Banking information',
+    '7' => 'Daugavpils Regional Christian Democratic Human Rights Movement, abbreviated DRCDP',
+    '8' => 'Feedback',
+    '9' => 'Your name',
+    '10' => 'Your e-mail',
+    '11' => 'Your phone',
+    '12' => 'Subject message',
+    '13' => 'Message',
+    '14' => 'Send',
+];
--- resources/views/contacts.blade.php
+++ resources/views/contacts.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app.app')
 
-@section('title')
+@section('title', __('contacts.h1'))
 
 @section('css')
     <link rel="stylesheet" href="/css/contacts.css" />
@@ -14,19 +14,19 @@
                 <div class="main__center main__desk">
                     <div class="contacts-content">
                         <h1 class="contacts-content__title title">
-                            <span class="contacts-content__title-text"> Контакты </span>
+                            <span class="contacts-content__title-text"> {{ __('contacts.h1') }} </span>
                         </h1>
                         <div class="contacts-content__outer outer">
                             <div class="contacts-content__inner inner">
                                 <div class="inner__wrapper wrapper">
                                     <h2 class="inner__subtitle visually-hidden">
                                         <!-- В макете нет. для читалок -->
-                                        Контактная информация
+                                        {!! __('contacts.1') !!}
                                     </h2>
                                     <ul class="wrapper__connections connections">
                                         <li class="connections__row">
                                             <div class="connections__column connections__column--title">
-                                                Электронная почта
+                                                {!! __('contacts.2') !!}
                                             </div>
                                             <div class="connections__column connections__column--data">
                                                 <i><a
@@ -36,7 +36,7 @@
 
                                         <li class="connections__row">
                                             <div class="connections__column connections__column--title">
-                                                Телефон в&nbsp;Латвии
+                                                {!! __('contacts.3') !!}
                                             </div>
                                             <div class="connections__column connections__column--data">
                                                 <a href="tel:+37129892296">(+371)29892296</a>
@@ -45,7 +45,7 @@
 
                                         <li class="connections__row">
                                             <div class="connections__column connections__column--title">
-                                                Телефон в&nbsp;Германии
+                                                {!! __('contacts.4') !!}
                                             </div>
                                             <div class="connections__column connections__column--data">
                                                 <a href="tel:+4915221942007">(+49)15221942007</a>
@@ -54,7 +54,7 @@
 
                                         <li class="connections__row">
                                             <div class="connections__column connections__column--title">
-                                                Офис разработки
+                                                {!! __('contacts.5') !!}
                                             </div>
                                             <div class="connections__column connections__column--data">
                                                 <address>Grebensteiner str 2. Kassel . 34127. Germany</address>
@@ -63,11 +63,10 @@
                                     </ul>
                                     <div class="wrapper__bank-data bank-data">
                                         <h2 class="inner__subtitle">
-                                            Банковские данные
+                                            {!! __('contacts.6') !!}
                                         </h2>
                                         <p class="inner__descr">
-                                            Даугавпилское Региональное Христианско Демократическое Правозащитное
-                                            Движение, сокращённое наименование&nbsp;&mdash; ДРХДПД
+                                            {!! __('contacts.7') !!}
                                         </p>
                                         <ul class="bank-data__list">
                                             <li class="bank-data__item">
@@ -92,23 +91,23 @@
                                     <!-- JS для этой формы находится в html внизудокумента -->
                                     <form id="feedback-form" class="wrapper__feedback-form feedback-form">
                                         <h2 class="inner__subtitle feedback-form__subtitle">
-                                            Обратная связь
+                                            {{ __('contacts.8') }}
                                         </h2>
                                         <div class="feedback-form__inputs-wrapper">
-                                            <input id="feedback-form-input-name" type="text" placeholder="Ваше имя*"
+                                            <input id="feedback-form-input-name" type="text" placeholder="{{ __('contacts.9') }}*"
                                                 minlength="3" maxlength="20" required class="feedback-form__input">
-                                            <input id="feedback-form-input-email" type="email" placeholder="Ваш e-mail*"
+                                            <input id="feedback-form-input-email" type="email" placeholder="{{ __('contacts.10') }}*"
                                                 minlength="3" maxlength="50" required class="feedback-form__input">
-                                            <input id="feedback-form-input-tel" type="text" placeholder="Ваш телефон*"
+                                            <input id="feedback-form-input-tel" type="text" placeholder="{{ __('contacts.11') }}*"
                                                 minlength="3" maxlength="12" required class="feedback-form__input">
-                                            <input id="feedback-form-input-subject" type="text" placeholder="Тема"
+                                            <input id="feedback-form-input-subject" type="text" placeholder="{{ __('contacts.12') }}"
                                                 maxlength="50" class="feedback-form__input">
                                             <textarea id="feedback-form-input-message" name="feedback-form-input-message"
-                                                class="feedback-form__input feedback-form__input--textarea" placeholder="Сообщение" maxlength="5000"></textarea>
+                                                class="feedback-form__input feedback-form__input--textarea" placeholder="{{ __('contacts.13') }}" maxlength="5000"></textarea>
                                         </div>
                                         <button id="feedback-form-button-submit" type="submit"
                                             class="feedback-form__button">
-                                            Отправить
+                                            {{ __('contacts.14') }}
                                         </button>
                                     </form>
                                 </div>
