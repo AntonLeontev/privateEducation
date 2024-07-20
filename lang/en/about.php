8ed465a23f993a52107bc0fcc96b35c86bfcacba
Anton <aner-anton@yandex.ru>
2024-07-20 13:20:01 +0300

    add: about translation

---
+++ lang/en/about.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'h1' => 'About project',
+    '1' => 'The Private Education Project',
+    '2' => 'Presented as an e-book, audiobook, and video-book.',
+    '3' => 'Preamble',
+    '4' => 'The essence of innovative education is the qualified assistance of Development in the child.',
+    '5' => 'I. The Art of Preserving Mental and Physical Health.',
+    '6' => 'II. Preventing legal conflicts – to keep a child from committing criminal offenses and ending up in a
+juvenile detention center or prison.',
+    '7' => 'III. Adequate mental and physical Growth.',
+    '8' => 'IV. Religious and Spiritual Self-Identification.',
+    '9' => 'V. Adequate sexual self-identification.',
+    '10' => 'VI. Political and Professional-Technological Self-Identification.',
+    '11' => 'VII. Domestic Culture.',
+    '12' => 'a. Quality of nutrition.',
+    '13' => 'b. Quality of underwear, clothing, and shoes.',
+    '14' => 'c. Quality and comfort of housing space.',
+    '15' => 'd. Quality of the Environment.',
+    '16' => 'The strategic goal of this Educational Project is Preserving, Strengthening, and Developing the
+priceless Transcendental Gift – our ability and capacity to be Human',
+    '17' => 'And to define Strategic Objectives as Developing Freedom of Personality and Individuality through
+adequate cognition and having a positive experience of Reality.',
+    '18' => 'This intellectual product is presented in a digital form and uses modern information technology to
+promote and optimize the end-customer’s perception of this product.',
+    '19' => 'When you activate the Internet page:',
+    '20' => 'www.private-new-education.com',
+    '21' => 'you will get an option to get free access to video files for all the 17 fragments for a brief review
+of the Presentation.',
+];
--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -15,54 +15,50 @@
                 <div class="main__center main__desk">
                     <div class="about-content">
                         <h1 class="about-content__title title">
-                            <span class="about-content__title-text"> О проекте </span>
+                            <span class="about-content__title-text"> {{ __('about.h1') }} </span>
                         </h1>
                         <div class="about-content__outer outer">
                             <div class="about-content__inner inner">
-                                <h2 class="inner__subtitle"> Проект Частное образование </h2>
+                                <h2 class="inner__subtitle"> {{ __('about.1') }} </h2>
                                 <span class="inner__desclaimer">
-                                    Представлен в электронной книге, в аудиокниге и в видеокниге
+                                    {{ __('about.2') }}
                                 </span>
-                                <span class="inner__preamble"> Преамбула </span>
+                                <span class="inner__preamble"> {{ __('about.3') }} </span>
                                 <span class="inner__descr">
-                                    Суть инновационного образования, заключается в квалифицированном
-                                    содействии, <br />Развитии у ребёнка
+                                    {{ __('about.4') }}
                                 </span>
 
                                 <ul class="inner__list">
                                     <li class="inner__item">
-                                        I. Искусства сохранения, психического и физического здоровья.
+                                        {{ __('about.5') }}
                                     </li>
                                     <li class="inner__item">
-                                        II. Профилактика юридических конфликтов – уберечь ребенка от
-                                        совершения уголовно – наказуемых правонарушений и попадания в
-                                        детскую колонию или тюрьму.
+                                        {{ __('about.6') }}
                                     </li>
                                     <li class="inner__item">
-                                        III. Адекватного психического и физического Роста.
+                                        {{ __('about.7') }}
                                     </li>
                                     <li class="inner__item">
-                                        IV. Религиозно – Духовной Самоиндефикации.
+                                        {{ __('about.8') }}
                                     </li>
                                     <li class="inner__item">
-                                        V. Адекватной сексуальной самоиндефикации.
+                                        {{ __('about.9') }}
                                     </li>
                                     <li class="inner__item">
-                                        VI. Политической и Профессионально – технологической
-                                        Самоиндефикации.
+                                        {{ __('about.10') }}
                                     </li>
                                     <li class="inner__item additional">
-                                        VII. Бытовой культуры.
+                                        {{ __('about.11') }}
                                         <ul class="additional__list">
-                                            <li class="additional__item">a. Качество питания.</li>
+                                            <li class="additional__item">{{ __('about.12') }}</li>
                                             <li class="additional__item">
-                                                b. Качество белья, одежды, и обуви.
+                                                {{ __('about.13') }}
                                             </li>
                                             <li class="additional__item">
-                                                c. Качество и комфорт жилищного пространства.
+                                                {{ __('about.14') }}
                                             </li>
                                             <li class="additional__item">
-                                                d. Качество Экологии Окружающей среды.
+                                                {{ __('about.15') }}
                                             </li>
                                         </ul>
                                     </li>
@@ -70,38 +66,28 @@
                                 <ul class="inner__target-list target-list">
                                     <li class="target-list__item">
                                         <p class="target-list__descr">
-                                            Стратегическая Цель данного Образовательного проекта –
-                                            Сохранение, Укрепление и Развитие бесценного
-                                            Трансцендентального Дара как возможность и способность быть
-                                            Человеком.
+                                            {{ __('about.16') }}
                                         </p>
                                     </li>
                                     <li class="target-list__item">
                                         <p class="target-list__descr">
-                                            А Стратегические Задачи определить как – Развитие Свободы
-                                            Личности и Индивидуальности, через адекватное познание и опыт
-                                            позитивного переживания Реальности.
+                                            {{ __('about.17') }}
                                         </p>
                                     </li>
                                     <li class="target-list__item">
                                         <p class="target-list__descr">
-                                            Интеллектуальный продукт представлен в цифровом формате и
-                                            использует современные информационные технологии для
-                                            популяризации и оптимизации восприятия этого продукта конечным
-                                            потребителем.
+                                            {{ __('about.18') }}
                                         </p>
                                         <p class="target-list__descr">
-                                            При активации интернет-страницы:
+                                            {{ __('about.19') }}
                                             <i>
-                                                <a href="https://www.private-new-education.de">www.private-new-education.de
+                                                <a href="https://www.private-new-education.com" rel="nofollow">{{ __('about.20') }}
                                                 </a></i>
                                         </p>
                                     </li>
                                     <li class="target-list__item">
                                         <p class="target-list__descr">
-                                            Появляется возможность бесплатного доступа к видеофайлам всех
-                                            17 – фрагментов для краткого ознакомления в качестве
-                                            Презентации.
+                                            {{ __('about.21') }}
                                         </p>
                                     </li>
                                 </ul>
