d6118707d61f1233fc39742b3acd0560853f2436
Anton <aner-anton@yandex.ru>
2024-12-15 18:50:40 +0300

    fix: amounts in admin

--- resources/views/components/user.blade.php
+++ resources/views/components/user.blade.php
@@ -112,7 +112,7 @@
 												<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 													<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 												</svg>
-												<span x-text="fragment.audio?.price + ' €'"></span>
+												<span x-text="fragment.audio?.price.amount + ' €'"></span>
 											</div>
 						
 											<div class="flex items-center gap-x-1" 
@@ -142,7 +142,7 @@
 												<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 													<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 												</svg>
-												<span x-text="fragment.video?.price + ' €'"></span>
+												<span x-text="fragment.video?.price.amount + ' €'"></span>
 											</div>
 						
 											<div class="flex items-center gap-x-1" 
@@ -211,7 +211,7 @@
 															<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 																<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 															</svg>
-															<span x-text="fragment.audio?.price + ' €'"></span>
+															<span x-text="fragment.audio?.price.amount + ' €'"></span>
 														</div>
 									
 														<div class="flex items-center gap-x-1" 
@@ -241,7 +241,7 @@
 															<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
 																<path stroke-linecap="round" stroke-linejoin="round" d="M14.25 7.756a4.5 4.5 0 100 8.488M7.5 10.5h5.25m-5.25 3h5.25M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 															</svg>
-															<span x-text="fragment.video?.price + ' €'"></span>
+															<span x-text="fragment.video?.price.amount + ' €'"></span>
 														</div>
 									
 														<div class="flex items-center gap-x-1" 
