855f6ee58c4a03919185f4693c9eab6721cbfd5d
Anton <aner-anton@yandex.ru>
2024-12-21 18:43:40 +0300

    fix: changing media type

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -120,24 +120,31 @@
                         muted: true,
                     });
         
-                    this.player.on('ended', () => {
-                        this.playingFragment = this.fragments[this.playingFragment.id === 17 ? 0 : this.playingFragment.id]
-						this.startPlay(this.playingMedia)
+                    this.player.on('ended', () => this.playNext());
 
-						@if (auth()->check()) 
-							axios
-								.post(route('presentation-view.store'), {
-									'presentation_id': this.playingFragment.id,
-									'is_reading': false,
-									'is_passive': true,
-									'lang': '{{ loc() }}',
-								})
-						@endif
-                    });
+					this.player.on('error', () => {
+						if (this.playingMedia === 'presentation') {
+							this.playNext()
+						}
+					});
         
 					this.startPlay(this.playingMedia)
                 })
             },
+			playNext() {
+				this.playingFragment = this.fragments[this.playingFragment.id === 17 ? 0 : this.playingFragment.id]
+				this.startPlay('presentation')
+
+				@if (auth()->check()) 
+					axios
+						.post(route('presentation-view.store'), {
+							'presentation_id': this.playingFragment.id,
+							'is_reading': false,
+							'is_passive': true,
+							'lang': '{{ loc() }}',
+						})
+				@endif
+			},
             activateFragment(id) {
                 this.selectedFragment = this.fragments
                     .find(el => el.id == id)
@@ -219,9 +226,10 @@
 			},
 			startPlay(mediaType) {
 				let sound = this.sound === 'text' ? 'stereo' : this.sound
+				this.playingMedia = mediaType
 
 				this.player.src({
-					type: this.playingFragment[mediaType].media[0].format,
+					type: this.playingFragment[mediaType].media[0]?.format,
 					src: `/media/${mediaType}/${this.playingFragment.id}/{{ loc() }}/${sound}/${this.device}` 
 				})
 				this.player.play()
@@ -536,132 +544,6 @@
                         </button>
                     </div>
                 </div>
-
-                {{-- <div id="dialog-login" class="dialog-login popup-dialog-hidden green">
-                    <div class="dialog__top">
-                        <h4>Полное содержание. Фрагмент №8</h4>
-                        <button class="dialog__close"></button>
-                    </div>
-                    <div class="dialog__center">
-                        <div id="account-autorization" class="dialog__autorization autorization">
-                            <h3 class="autorization__title">Авторизация</h3>
-                            <div class="autorization__button-box">
-                                <button id="autorization-mode-btn-autorization"
-                                    class="autorization__btn autorization__btn--autorization">
-                                    <img src="img/user.png" alt="user" class="autorization__img" />
-                                    <span class="autorization__btn-text">Я&nbsp;пользователь</span>
-                                </button>
-                                <button id="autorization-mode-btn-registration"
-                                    class="autorization__btn autorization__btn--registration">
-                                    <img src="img/user.png" alt="user"
-                                        class="autorization__img autorization__img--grey" />
-                                    <span class="autorization__btn-text">Регистрация</span>
-                                </button>
-                            </div>
-                            <form id="autorization-form" class="autorization__form">
-                                <span class="autorization__label">
-                                    Адрес электронной почты:
-                                </span>
-                                <input id="autorization-email-input" type="email" class="autorization__input"
-                                    placeholder="* * * * * * * * * * * * * * * * *" />
-                                <div class="autorization__wrapper">
-                                    <span class="autorization__label"> Пароль: </span>
-                                    <button id="autorization-password-recall-btn" class="autorization__link">
-                                        Забыли пароль?
-                                    </button>
-                                </div>
-                                <input id="autorization-password-input" type="password" class="autorization__input"
-                                    placeholder="* * * * * * * * * * * * * * * * *" />
-                                <span id="autorization-error-msg" class="autorization__error-msg">Не удалось войти в
-                                    аккаунт,
-                                    введенные e-mail или
-                                    пароль неверны</span>
-                                <button id="autorization-submit-btn" class="autorization__submit-btn">
-                                    ВХОД
-                                </button>
-                            </form>
-                        </div>
-                        <div id="account-registration" class="dialog__registration registration">
-                            <h3 class="registration__title">Регистрация</h3>
-                            <div class="registration__button-box">
-                                <button id="registration-mode-btn-aurorization"
-                                    class="registration__btn registration__btn--autorization">
-                                    <img src="img/user.png" alt="user"
-                                        class="registration__img registration__img--grey" />
-                                    <span class="registration__btn-text">Авторизация</span>
-                                </button>
-                                <button id="registration-mode-btn-registration"
-                                    class="registration__btn registration__btn--registration">
-                                    <img src="img/user.png" alt="user" class="registration__img" />
-                                    <span class="registration__btn-text">Регистрация</span>
-                                </button>
-                            </div>
-                            <form id="registration-form" class="registration__form">
-                                <span class="registration__label">
-                                    Адрес электронной почты:
-                                </span>
-                                <input id="registration-email-input" type="email" class="registration__input"
-                                    placeholder="* * * * * * * * * * * * * * * * *" />
-                                <button id="registration-submit-btn" class="registration__submit-btn">
-                                    ПРОДОЛЖИТЬ
-                                </button>
-                            </form>
-                        </div>
-                        <div id="account-password-recall" class="dialog__password-recall password-reacll">
-                            <h3 class="password-reacll__title">Восстановить пароль</h3>
-
-                            <form id="password-reacll-form" class="password-reacll__form">
-                                <span class="password-reacll__label">
-                                    Адрес электронной почты:
-                                </span>
-                                <input id="password-reacall-input" type="email" class="password-reacll__input"
-                                    placeholder="* * * * * * * * * * * * * * * * *" />
-                                <button id="password-reacll-submit-btn" class="password-reacll__submit-btn">
-                                    ВОССТАНОВИТЬ
-                                </button>
-                            </form>
-                        </div>
-                        <div id="account-password-info-msg" class="dialog__password-info-msg password-info-msg">
-                            <div class="password-info-msg__wrapper">
-                                <p class="password-info-msg__text">
-                                    Для продолжения покупки перейдите по&nbsp;ссылке
-                                    в&nbsp;письме и&nbsp;подтвердите e-mail
-                                </p>
-                            </div>
-                        </div>
-                        <div id="account-password-new-info-msg"
-                            class="dialog__password-new-info-msg password-new-info-msg">
-                            <div class="password-new-info-msg__wrapper">
-                                <p class="password-new-info-msg__text">
-                                    На&nbsp;вашу почту отправлен новый пароль для входа
-                                    на&nbsp;сервис
-                                </p>
-                            </div>
-                            <button id="password-new-info-msg-btn" class="password-new-info-msg__btn">
-                                АВТОРИЗАЦИЯ
-                            </button>
-                        </div>
-                    </div>
-                </div> --}}
-
-                {{-- <div id="dialog1" class="continue">
-					<div class="dialog__top">
-						<h4>Полное содержание. Фрагмент №8</h4>
-						<button class="dialog__close">
-
-						</button>
-					</div>
-					<div class="dialog__center">
-
-						<div class="dialog__center-wrapper">
-							<div class="dialog__bg">
-								<h5>Для продолжения покупки перейдите<br/> по ссылке в письме и подтвердите e-mail</h5>
-							</div>
-							
-
-						</div>
-					</div>
-				</div> --}}
 
                 <div class="continue popup-dialog" x-show="modal === 'step1'" x-cloak>
                     <div class="dialog__top">
