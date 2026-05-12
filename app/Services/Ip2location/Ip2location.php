c5387e68197b2fd6cbf0a4b77fd55329093adc3f
Anton <aner-anton@yandex.ru>
2026-05-12 13:46:06 +0300

    cache ip2location

--- app/Services/Ip2location/Ip2location.php
+++ app/Services/Ip2location/Ip2location.php
@@ -4,6 +4,7 @@ namespace App\Services\Ip2location;
 
 use App\Services\Ip2location\Exceptions\Ip2locationException;
 use Illuminate\Http\Client\Response;
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Http;
 
 class Ip2location
@@ -12,15 +13,19 @@ class Ip2location
 
     public function check(string $ip = ''): Ip2locationDTO
     {
-        $response = Http::baseUrl(self::BASE_URL)
-            ->throw(function (Response $response) {
-                throw new Ip2locationException($response);
-            })
-            ->get('/', [
-                'key' => config('services.ip2location.key'),
-                'ip' => $ip,
-            ]);
+        $cacheKey = 'ip2location:'.hash('sha256', $ip);
 
-        return Ip2locationDTO::fromResponse($response);
+        return Cache::remember($cacheKey, now()->addDays(3), function () use ($ip) {
+            $response = Http::baseUrl(self::BASE_URL)
+                ->throw(function (Response $response) {
+                    throw new Ip2locationException($response);
+                })
+                ->get('/', [
+                    'key' => config('services.ip2location.key'),
+                    'ip' => $ip,
+                ]);
+
+            return Ip2locationDTO::fromResponse($response);
+        });
     }
 }
